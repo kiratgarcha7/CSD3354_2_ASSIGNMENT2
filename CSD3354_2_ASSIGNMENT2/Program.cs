b5f4736f13d89b46da2305dffaae82e46bd873a0
visualstudio <visualstudio@125-PC07>
2019-04-13 20:12:27 -0400

    commited first

--- CSD3354_2_ASSIGNMENT2/Program.cs
+++ CSD3354_2_ASSIGNMENT2/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace CSD3354_2_ASSIGNMENT2
 {
-    class Program
+    public class program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            DelegateExercises a = new DelegateExercises();
+            a.Method2();
+        }
+    }
+    public class DelegateExercises
+    {
+        // Student Name: Jatinder Singh  Student ID: C0725866
+        // Student Name: Arshdeep Singh Student ID: C0730385
+
+        public delegate void MyDelegate();
+
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
         }
     }
 }
