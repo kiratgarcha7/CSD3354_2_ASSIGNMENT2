cbb4e9108e9b4de3db341c99ad3b90699e702a0d
visualstudio <visualstudio@125-PC07>
2019-04-13 20:23:33 -0400

    Use of unassigned local variable 'myDelegate'

--- CSD3354_2_ASSIGNMENT2/Program.cs
+++ CSD3354_2_ASSIGNMENT2/Program.cs
@@ -6,34 +6,36 @@ using System.Threading.Tasks;
 
 namespace CSD3354_2_ASSIGNMENT2
 {
+    using System;
+    delegate void ExampleDelegate(string xyz);
 
-
-    public class program
+    class Program
     {
-
-        static void Main()
+        public static void Method1(string xyz)
         {
+            Console.WriteLine(xyz + "Method1");
+        }
+        public static void Method2(string xyz)
+        {
+            Console.WriteLine(xyz + "Method2");
+        }
+        public static void Main()
+        {
+            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
 
-
-
-            DelegateExercises a = new DelegateExercises();
-            a.Method3();
+            ex1Delegate = new ExampleDelegate(Method1);
+            ex2Delegate = new ExampleDelegate(Method2);
+            ex3Delegate = ex1Delegate + ex2Delegate;
+            ex1Delegate("AAA");
+            ex2Delegate("BBB");
+            ex3Delegate("CCC");
+            myDelegate("DDD");
+            myDelegate = ex3Delegate - ex1Delegate;
+            myDelegate("EEE");
+            myDelegate = ex3Delegate - ex2Delegate;
+            myDelegate("FFF");
             Console.ReadLine();
+
         }
     }
-    public delegate void MyDelegate();
-    public class DelegateExercises
-    {
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
-        }
-        public void Method3()
-        {
-
-            System.Console.WriteLine(MyDelegate.ToString());
-        }
-    }
-
-
 }
