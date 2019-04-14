42b230e575b8d166881a7ab8eef5ae2d4addca72
visualstudio <visualstudio@125-PC07>
2019-04-13 20:18:31 -0400

    passing method1 as argument of method4 using delegate.

--- CSD3354_2_ASSIGNMENT2/Program.cs
+++ CSD3354_2_ASSIGNMENT2/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment_2
+namespace CSD3354_2_ASSIGNMENT2
 {
 
     public class program
@@ -12,10 +12,8 @@ namespace Assignment_2
 
         static void Main()
         {
-            // Student Name: Sukhdeep Singh  Student ID: C0725204
-            // Student Name: Gurkirat Singh Student ID: C0731773
-            // CSD3354 Section 2
-            // Assignment 2
+
+
 
             DelegateExercises a = new DelegateExercises();
             a.Method3();
@@ -29,22 +27,25 @@ namespace Assignment_2
 
         public delegate int MyDelegate(int intValue);
 
-        public int Method1(int intMethod1)
+        int Method1(int intMethod1)
         {
             return intMethod1 * 2;
         }
-        public int Method2(int intMethod2)
+        int Method2(int intMethod1)
         {
-            return intMethod2 * 10;
+            return intMethod1 * 10;
+        }
+        public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            Console.WriteLine(result);
         }
         public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            int result1 = myDelegate(10);
-            System.Console.WriteLine(result1);
+            Method4(myDelegate);
             myDelegate = new MyDelegate(Method2);
-            int result2 = myDelegate(10);
-            System.Console.WriteLine(result2);
+            Method4(myDelegate);
         }
+
     }
-}
